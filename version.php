9ae723122f60fa116516cdcbf32ff4d6598c3b2d
Mark Sharp <m.sharp@chi.ac.uk>
2020-01-28 09:19:30 +0000

    Checked for Moodle 3.8

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017111301;
+$plugin->version = 2017111302;
 $plugin->requires = 2017111302; // Version 3.4.2.
 $plugin->release = 1.2;
 $plugin->maturity = MATURITY_STABLE;
